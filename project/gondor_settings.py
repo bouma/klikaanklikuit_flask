17b78286b564140f4d1acc35dc6429d2d3260822
Martin Bouma <bouma@egoactive.com>
2015-03-03 00:03:45 +0100

    hardware v2

--- project/gondor_settings.py
+++ project/gondor_settings.py
@@ -11,3 +11,6 @@ SWITCHES = [('Linker lamp', 'G 12'),
 # switch subsets (point to the indexes of the SWITCHES array)
 LIGHTSONLY = [0,2,3]
 ALL = range(0, len(SWITCHES))
+
+SPARK_DEVICE_ID = "48ff70065067555057352287"
+SPARK_ACCESS_TOKEN = "ed33d6d63db5b2bb1f11d11ec767b5ccebf6ac18"
