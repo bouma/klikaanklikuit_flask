65f14f3e63875e34db5715f3feec64e65795894d
Martin <martin.bouma@gmail.com>
2018-02-18 20:47:21 +0000

    ding erbij + spark settings uit. gebruiken we nu niet

--- project/default_settings.py
+++ project/default_settings.py
@@ -1,5 +1,4 @@
-# HOST='192.168.1.61'
-HOST='0.0.0.0'
+HOST='192.168.178.11'
 CMND_FILE='/usr/local/bin/pilight-control'
 DRY_RUN=False
 DEBUG=True
@@ -9,7 +8,8 @@ SWITCHES = (('Linker lamp', '-d LinksBoven -s'),
             ('Boekenkast licht', '-d BoekenkastBoven -s'),
             ('Hanglamp Boven', '-d HanglampBoven -s'),
             ('TV Beneden', '-d TVBeneden -s'),
-            ('Lotek Beneden', '-d BankBeneden -s'))
+            ('Lotek Beneden', '-d BankBeneden -s'),
+            ('Ventilator Zolder', '-d VentilatorZolder -s'))
 
 # switch subsets (point to the indexes of the SWITCHES array)
 LIGHTSONLY = [0, 2, 3]
@@ -17,5 +17,6 @@ BOVENONLY = [0, 1, 2, 3]
 BENEDENONLY = [4, 5]
 ALL = range(0, len(SWITCHES))
 
-SPARK_DEVICE_ID = "48ff70065067555057352287"
-SPARK_ACCESS_TOKEN = "b66240ea89175ab476b6942115d74a3740bb7043"
+# SPARK_DEVICE_ID = "48ff70065067555057352287"
+# SPARK_ACCESS_TOKEN = "b66240ea89175ab476b6942115d74a3740bb7043"
+# SPARK_ACCESS_TOKEN = "922528b46a15ba2d7cc0bbeb2d8c8d6da137f5bd"
